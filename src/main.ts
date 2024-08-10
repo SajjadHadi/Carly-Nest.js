8ea9eae77893792f5a279fc2844183c1b9ab68b5
SajjadHadi <sajjadhadi77@gmail.com>
2024-08-10 16:50:09 +0200

    Add cors and accept some origins.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: /^http:\/\/localhost:\d+$/,   
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, 
+      });
 
     const config = new DocumentBuilder()
         .setTitle('Carly')
